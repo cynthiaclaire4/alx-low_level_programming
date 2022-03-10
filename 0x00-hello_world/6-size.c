f33969e33a4fd47672351e6f5df4412c632a49f3
cynthiaclaire4 <98520514+cynthiaclaire4@users.noreply.github.com>
2022-03-11 00:08:47 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,30 +1,16 @@
-#include <stdio.h>                                                                                                                                                      
+#include <stdio.h>
 
-                                                                                                                                                                        
-/**                                                                                                                                                                     
+/**
+*main - it runs the function
+*Return: returns 0 as success
+*/
 
- * main - print the size of the various types                                                                                                                           
-
- *                                                                                                                                                                      
-
- * REturn: Always 0 (Success)                                                                                                                                           
-
- */                                                                                                                                                                     
-
-                                                                                                                                                                        
-int main(void)                                                                                                                                                          
-
-{                                                                                                                                                                       
-
-        printf("Size of a char: %d byte(s) \n", (int) sizeof(char));                                                                                                    
-
-        printf("Size of a int: %d byte(s) \n", (int) sizeof(int));                                                                                                      
-
-        printf("Size of a long int: %d byte(s) \n", (int) sizeof(long int));                                                                                            
-
-        prinf("Size of a long long int: %d byte(s) \n", (int) sizeof(long long int));                                                                                   
-
-        printf("Size of a float: %d byte(s) \n", (int) sizeof(float));
-	return (0)
-
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
