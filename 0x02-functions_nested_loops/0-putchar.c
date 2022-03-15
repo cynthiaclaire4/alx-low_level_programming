4883b74b3c77ea4e3bf7ed7584927ff7d269ac89
root <cynthiaclairechisom@gmail.com>
2022-03-15 13:31:03 -0700

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include <stdio>
 #include "main.h"
 /**
- * main - Entry point
+ * main - Main block
  * Description: print _putchar
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-{
-	char *sh = "_putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	putchar('\n');
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
+
