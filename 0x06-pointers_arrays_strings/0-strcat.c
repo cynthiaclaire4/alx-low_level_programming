63673f9a008987a9c6228c2e973b83354390a2fa
root <cynthiaclairechisom@gmail.com>
2022-03-24 07:54:58 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcat - appends string and then adds a terminating null byte
+ * @dest: willtake the value
+ * @src: will be appended to dest
+ * at the end of dest, and then adds a terminating null byte
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	_strcat(dest, src);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * *_strncpy copies a string
+ * returns: (0) success
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	*src = n;
+	*dest = n
