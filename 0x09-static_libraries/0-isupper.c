7e78ce31d98d3e1751f6b011e98a35fbe4968f5e
root <cynthiaclairechisom@gmail.com>
2022-04-03 04:35:05 -0700

    staticlibrary

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,10 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
+ * _isupper - checks for uppercase charater
  * @c: the character to be checked
- * Return: 1 if cis uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
