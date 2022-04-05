beb91f7c7f3dca5dcb7dc58d4e1ffb5534f3f21c
root <cynthiaclairechisom@gmail.com>
2022-04-05 03:30:53 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array -  creates an array of chars
+ * @size: size of the array.
+ * @c: storaged for char
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *cr;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cr[i] = c;
+	return (cr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+directoty on malloc and free.
